60eadcfb14a9f3cb34bf0bf414e4dd14696fb81d
Semiranov <semiranov@gmail.com>
2023-11-01 21:25:07 +0300

    cl_work1

---
+++ 001/ClassWork/001.py
@@ -0,0 +1,10 @@
+LOWER_LIMIT = 2
+UPPER_LIMIT = 10
+COLUMN = 4
+
+for i in LOWER_LIMIT, COLUMN + LOWER_LIMIT:
+    for j in range(LOWER_LIMIT, UPPER_LIMIT + 1):
+        for k in range(i, i + COLUMN):
+            print(f'{k:>2}*{j:>2}={k*j:>2}', end='\t')
+        print()
+    print()
